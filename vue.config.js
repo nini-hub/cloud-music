d9b26ff849c0539ff9415bfb0ee17f3825cf38ce
hualalalalalala <490291490@qq.com>
2020-12-07 15:35:54 +0800

    修复预渲染移动端适配问题

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
 module.exports = {
   configureWebpack: {
     module: {
@@ -31,7 +33,24 @@ module.exports = {
       ],
       useRenderEvent: true,
       headless: true,
-      onlyProduction: true
+      onlyProduction: true,
+      postProcess: route => {
+        // 预渲染内容写入之前的额外操作
+        const reg = /<meta name="viewport".*user-scalable=no">/gi
+        const res = route.html.match(reg)
+        route.html = route.html.replace(res[1], '')
+
+        // 1.根据字符串创建一个网页
+        const html = new JSDOM(route.html)
+        // 2.从创建好的网页中拿到document对象
+        const dom = html.window.document
+        // 3.找到对应的元素, 删除对应的元素
+        const loadingEle = dom.querySelector('.container')
+        dom.body.removeChild(loadingEle)
+
+        route.html = html.serialize()
+        return route
+      }
     }
   }
 }
